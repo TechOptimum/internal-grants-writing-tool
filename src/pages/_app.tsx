4816bc8f1500f6bca9ed531e5e429de744536474
Rayane Benamre <benamrerayane@gmail.com>
2023-07-28 15:07:51 +0100

    Implemented Clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "~/styles/theme";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <ClerkProvider>
+        <Component {...pageProps} />
+      </ClerkProvider>
     </ChakraProvider>
   );
 };
