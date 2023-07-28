1e85bb0ce773a76b3f7fb59f3f1fe7ea289f77ae
Rayane Benamre <benamrerayane@gmail.com>
2023-07-28 12:46:18 +0100

    Set up Chakra UI and removed default T3 styles/content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
-import "~/styles/globals.css";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "~/styles/theme";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
