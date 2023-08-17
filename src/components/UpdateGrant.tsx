a60783cff75195169a79606ce5d5394896ecc413
Hemit Patel <itshp404@gmail.com>
2023-08-16 21:35:23 -0400

    changed filename to make more sense, fixed problems with update modal to add new fields

--- src/components/UpdateGrant.tsx
+++ src/components/UpdateGrant.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {
-  Box,
+  Select,
   Button,
   FormControl,
   FormLabel,
@@ -11,8 +11,10 @@ import {
   Text,
   Flex,
   Divider,
+  useToast
 } from '@chakra-ui/react';
 import { AiFillFileText } from 'react-icons/ai';
+import {CloseIcon, CheckIcon} from '@chakra-ui/icons'
 import Head from 'next/head';
 import { api } from '~/utils/api';
 
@@ -22,23 +24,34 @@ interface CreateGrantProps {
   initialAmount: number;
   initialDescription: string;
   initialCriteria: string;
+  initialEndDate: Date;
 }
 
-const CreateGrant = ({ grant_id, initialAmount, initialCriteria, initialDescription, initialTitle }: CreateGrantProps) => {
+const CreateGrant = ({
+  grant_id,
+  initialAmount,
+  initialCriteria,
+  initialDescription,
+  initialTitle,
+  initialEndDate,
+}: CreateGrantProps) => {
   const [title, setTitle] = useState(initialTitle);
   const [description, setDescription] = useState(initialDescription);
   const [amount, setAmount] = useState(initialAmount);
   const [criteria, setCriteria] = useState(initialCriteria);
+  const [endDate, setEndDate] = useState(initialEndDate);
+  const [available, setAvailable] = useState(Boolean);
+
+  const toast = useToast()
 
   const { refetch } = api.grants.getGrants.useQuery();
 
   const size = '3xl';
 
-
   const updateGrant = api.grants.updateGrant.useMutation({
     onSuccess: () => {
-      void refetch()
-    }
+      void refetch();
+    },
   });
 
   const resetForm = () => {
@@ -46,12 +59,50 @@ const CreateGrant = ({ grant_id, initialAmount, initialCriteria, initialDescript
     setDescription(initialDescription);
     setAmount(initialAmount);
     setCriteria(initialCriteria);
+    setEndDate(initialEndDate);
   };
+
+  const removeAvailability = () => {
+    toast({
+      title: "Setted your changes to the availablity to false",
+      status: "info",
+      duration: 5000,
+      isClosable: true,
+    });
+    setAvailable(false)
+  }
+
+  const addAvailability = () => {
+    toast({
+      title: "Setted your changes to the availablity to true",
+      status: "info",
+      duration: 5000,
+      isClosable: true,
+    });
+    setAvailable(true)
+  }
 
   const handleUpdateClick = async (id: string) => {
     try {
-      if (!title || !amount || !description || !criteria) {
-        return;
+      
+      const states = [
+        title,
+        description,
+        endDate,
+        amount,
+        criteria,
+      ];
+
+      for(const i of states) {
+        if(!i) {
+          toast({
+            title: "There are field(s) that are missing text, please fill them out ",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+          });
+          return;
+        }
       }
 
       await updateGrant.mutateAsync({
@@ -59,10 +110,17 @@ const CreateGrant = ({ grant_id, initialAmount, initialCriteria, initialDescript
         amount,
         description,
         criteria,
-        id
+        id,
+        endDate,
+        available,
       });
 
-      alert('Updated grant')
+      toast({
+        title: "Updated grant successfully",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
     } catch (error) {
       console.error('Error creating grant:', error);
     }
@@ -73,63 +131,80 @@ const CreateGrant = ({ grant_id, initialAmount, initialCriteria, initialDescript
       <Head>
         <title>Grant Update | TechOptimum Grants Writing Tool</title>
       </Head>
-        <Flex align="center" marginBottom="1rem">
-          <Text fontSize={size} fontWeight="bold">
-            Update your Grant
-          </Text>
-        </Flex>
-        <Text marginBottom={4} color="gray">
-          Update your grant as you wish!
+      <Flex align="center" marginBottom="1rem">
+        <Text fontSize={size} fontWeight="bold">
+          Update a Grant
         </Text>
-        <FormControl isRequired>
-          <FormLabel>Title</FormLabel>
-          <InputGroup>
-            <InputLeftAddon children={<AiFillFileText />} />
-            <Input
-              type="text"
-              placeholder="Update the title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </InputGroup>
-        </FormControl>
-        <FormControl marginTop={4} isRequired>
-          <FormLabel>Amount</FormLabel>
-          <InputGroup>
-            <InputLeftAddon children='$' />
-            <Input
-              type="number"
-              placeholder="Update the amount"
-              value={amount}
-              onChange={(e) => setAmount(parseFloat(e.target.value))}
-            />
-          </InputGroup>
-        </FormControl>
-        <FormControl marginTop={4} isRequired>
-          <FormLabel>Description</FormLabel>
-          <Textarea
-            placeholder="Update the description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
+      </Flex>
+      <Text marginBottom={4} color="gray">
+        Update the grant as you wish!
+      </Text>
+      <FormControl isRequired>
+        <FormLabel>Title</FormLabel>
+        <InputGroup>
+          <InputLeftAddon children={<AiFillFileText />} />
+          <Input
+            type="text"
+            placeholder="Update the title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
-        </FormControl>
-        <FormControl marginTop={4} isRequired>
-          <FormLabel>Criteria</FormLabel>
-          <Textarea
-            placeholder="Update the grant's criteria"
-            value={criteria}
-            onChange={(e) => setCriteria(e.target.value)}
+        </InputGroup>
+      </FormControl>
+      <FormControl marginTop={4} isRequired>
+        <FormLabel>Amount</FormLabel>
+        <InputGroup>
+          <InputLeftAddon children="$" />
+          <Input
+            type="number"
+            placeholder="Update the amount"
+            value={amount}
+            onChange={(e) => setAmount(parseFloat(e.target.value))}
           />
-        </FormControl>
-        <Divider my={6} />
-        <Flex justifyContent="flex-end" marginTop={8}>
-          <Button colorScheme="gray" marginRight={4} onClick={resetForm}>
-            Reset
-          </Button>
-          <Button colorScheme="blue" onClick={() => {handleUpdateClick(grant_id)}}>
-            Save
-          </Button>
-        </Flex>
+        </InputGroup>
+      </FormControl>
+      <FormControl marginTop={4} isRequired>
+        <FormLabel>Description</FormLabel>
+        <Textarea
+          placeholder="Update the description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </FormControl>
+      <FormControl marginTop={4} isRequired>
+        <FormLabel>Criteria</FormLabel>
+        <Textarea
+          placeholder="Update the grant's criteria"
+          value={criteria}
+          onChange={(e) => setCriteria(e.target.value)}
+        />
+      </FormControl>
+      <FormControl marginTop={4} isRequired>
+        <FormLabel>End Date</FormLabel>
+        <Input
+          type="date"
+          value={endDate ? new Date(endDate).toLocaleDateString('en-CA') : ''}
+          onChange={(e) => setEndDate(new Date(e.target.value))}
+        />
+      </FormControl>
+      <FormControl marginTop={4} isRequired>
+        <FormLabel>Toggle Availability</FormLabel>
+        <Button onClick={addAvailability} colorScheme='green' marginRight={2}> 
+          <CheckIcon />
+        </Button>
+        <Button onClick={removeAvailability} colorScheme='red'>
+          <CloseIcon />
+        </Button>
+      </FormControl>
+      <Divider my={6} />
+      <Flex justifyContent="flex-end" marginTop={8}>
+        <Button colorScheme="gray" marginRight={4} onClick={resetForm}>
+          Reset
+        </Button>
+        <Button colorScheme="blue" onClick={() => handleUpdateClick(grant_id)}>
+          Save
+        </Button>
+      </Flex>
     </>
   );
 };
