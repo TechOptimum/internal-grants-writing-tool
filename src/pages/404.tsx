abba1155b331685d9272f2021de95bc90c5c59e4
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2023-08-21 17:20:09 -0700

    something

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,16 @@
-import { Center } from '@chakra-ui/react'
-import Head from 'next/head'
-import React from 'react'
+import { Center } from "@chakra-ui/react";
+import Head from "next/head";
+import React from "react";
 
 const FourOhFour = () => {
   return (
     <>
-    <Head>
+      <Head>
         <title>Page not found</title>
-    </Head>
-    <Center>
-        404 - Page not found
-    </Center>
+      </Head>
+      <Center>404 - Page not found</Center>
     </>
-  )
-}
+  );
+};
 
-export default FourOhFour
+export default FourOhFour;
